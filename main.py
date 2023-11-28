c4ebb5c0412d6ddae4c65282f36b0d81f321c9ab
Gregory Ojiem <79064397+gro3228@users.noreply.github.com>
2023-11-28 18:27:54 -0500

    added project data

---
+++ main.py
@@ -0,0 +1,11 @@
+import open3d
+import os
+
+def main():
+    # load in point clouds
+    directory = "/dataset/PointClouds"
+    for
+    point_clouds = open3d.io.read_point_cloud()
+
+if __name__ == "__main__":
+    main()
