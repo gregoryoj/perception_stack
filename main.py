dc1a2d77633c1e7f1670122d75af8545b1c6b97a
Gregory Ojiem <79064397+gro3228@users.noreply.github.com>
2023-11-29 19:58:10 -0500

    Properly clustered vehicles and separated background from image
    Added proper clustering to each frame of the output visualization, and cropped out background and removed noise

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import open3d as o3d
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import open3d.geometry
+
+DISTANCE_TO_LAST_PCD = 0.001
 
 
-def preprocessing(point_clouds):
+def preprocessing(point_clouds): # todo fix the first frame having 0 points
+    point_clouds = point_clouds[1:]
     return point_clouds
 
 
@@ -17,7 +21,7 @@ def get_distances(last_point_clouds):
 
 
 def main():
-    image_to_display = 250
+    image_to_display = 125
     # load in point clouds
     path_to_data = "dataset/PointClouds"
     files = sorted(os.listdir(path_to_data), key=lambda x: int(x.split(".")[0]))
@@ -30,14 +34,17 @@ def main():
     for i in range(0, len(files)):
         path_to_point_cloud = path_to_data + '/' + files[i]
         pcd = o3d.io.read_point_cloud(path_to_point_cloud)
+        bbox = o3d.geometry.AxisAlignedBoundingBox.create_from_points(o3d.utility.Vector3dVector(np.asarray(pcd.points)))
+        bbox.max_bound = (bbox.max_bound[0], bbox.max_bound[1], bbox.min_bound[2] + 8)
+        pcd = open3d.geometry.PointCloud.crop(pcd, bbox)
 
         if i >= 5: last_point_clouds[0] = o3d.io.read_point_cloud(path_to_data + '/' + files[i - 5])
         last_point_clouds[1] = pcd
         distance_to_last_pcds = get_distances(last_point_clouds)
-        indexes_of_changed_points = np.where(distance_to_last_pcds > 0.00000001)[0]
+        indexes_of_changed_points = np.where(distance_to_last_pcds > DISTANCE_TO_LAST_PCD)[0]
         pcd_without_background = pcd.select_by_index(indexes_of_changed_points)
 
-        point_clouds.append(pcd)
+        point_clouds.append(pcd_without_background)
 
         count += 1
         if count > image_to_display + 1: break
@@ -53,36 +60,27 @@ def main():
     vis = o3d.visualization.Visualizer()
     vis.create_window()
 
-    # geometry is the point cloud used in your animaiton
-    geometry = point_clouds[1]
-    vis.add_geometry(geometry)
-
     time.sleep(2)
 
     for i in range(image_to_display):
-        geometry.points = point_clouds[i].points
-        vis.update_geometry(geometry)
+        vis.clear_geometries()
+
+        geometry = point_clouds[i]
+        with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Warning) as cm:
+            labels = np.array(geometry.cluster_dbscan(eps=10, min_points=3, print_progress=False))
+        max_label = labels.max()
+        print(f"point cloud has {max_label + 1} clusters")
+        colors = plt.get_cmap("cool")(labels / (max_label if max_label > 0 else 1))
+        colors[labels < 0] = 0
+
+        geometry.colors = o3d.utility.Vector3dVector(colors[:, :3])
+        vis.add_geometry(geometry)
         vis.poll_events()
         vis.update_renderer()
+
         time.sleep(0.10)
 
     vis.run()
-
-    # with o3d.utility.VerbosityContextManager(o3d.utility.VerbosityLevel.Debug) as cm:
-    #    labels = np.array(point_cloud.cluster_dbscan(eps=1, min_points=10, print_progress=True))
-
-
-"""    max_label = labels.max()
-    print(f"point cloud has {max_label + 1} clusters")
-    colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
-    colors[labels < 0] = 0
-    point_cloud.colors = o3d.utility.Vector3dVector(colors[:, :3])
-    o3d.visualization.draw_geometries([point_cloud],
-                                      zoom=0.455,
-                                      front=[-0.4999, -0.1659, -0.8499],
-                                      lookat=[2.1813, 2.0619, 2.0999],
-                                      up=[0.1204, -0.9852, 0.1215])
-"""
 
 if __name__ == "__main__":
     main()
